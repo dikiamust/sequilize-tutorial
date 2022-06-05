373a555a42629790d039dcc61dba25981d1abbff
dikiamust <agungmustaqimdiki@gmail.com>
2022-06-05 19:53:17 +0700

    add validation

--- modules/video/controllers/video.controller.js
+++ modules/video/controllers/video.controller.js
@@ -6,13 +6,19 @@ const Video = require('../../../models/Video');
 exports.addVideo = async (req, res) => {
   const { name, userId } = req.body
   try {
+    if (!name || !userId ){
+      return res.status(400).send({
+        message: 'Missing required field(s)!'
+      });
+    }
+
     const addVideo = await Video.create(req.body)
 
     res.json(addVideo);
     
   } catch (err) {
-    return res.status(400).send({
-      message: err
+    res.status(500).send({
+      err : 'Internal server error!'
     });
   }
 }
