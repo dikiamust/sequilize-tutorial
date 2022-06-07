980d49b5f8b16e0404d42f58d1127cca848bead8
Diki Agung Mustaqim <agungmustaqimdiki@gmail.com>
2022-06-07 16:24:13 +0700

    implement pug template engine

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const db = require('./db/db');
 const authRouter = require('./modules/auth/routes/auth.routes');
 const videoRouter = require('./modules/video/routes/video.routes')
 const morgan = require('morgan')
+const path = require('path')
 const notFoundRout = require('./utils/notFoundRout')
 const errorHandlerMiddleware = require('./utils/errorHandler')
 
@@ -12,8 +13,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan(':method :url :response-time :status'))
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
-app.get('/', (req, res) => res.send('Welcome to Api Sequelize Tutorial'))
+app.get('/', (req, res) => res.render("homepage"));
+
 
 app.use(authRouter)
 app.use(videoRouter)
