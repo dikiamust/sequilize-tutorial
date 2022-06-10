9df3e5859cd636b50eaae3992073c0fa2b9599a3
Diki Agung Mustaqim <agungmustaqimdiki@gmail.com>
2022-06-10 14:17:37 +0700

    fixing double quotes

--- index.js
+++ index.js
@@ -2,28 +2,28 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const db = require('./db/db');
 const authRouter = require('./modules/auth/routes/auth.routes');
-const videoRouter = require('./modules/video/routes/video.routes')
-const morgan = require('morgan')
-const path = require('path')
-const notFoundRout = require('./utils/notFoundRout')
-const errorHandlerMiddleware = require('./utils/errorHandler')
+const videoRouter = require('./modules/video/routes/video.routes');
+const morgan = require('morgan');
+const path = require('path');
+const notFoundRout = require('./utils/notFoundRout');
+const errorHandlerMiddleware = require('./utils/errorHandler');
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
-app.use(morgan(':method :url :response-time :status'))
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan(':method :url :response-time :status'));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', (req, res) => res.render("homepage"));
-app.get('/page/login', (req, res) => res.render("login_page"));
-app.get('/page/register', (req, res) => res.render("register_page"));
-app.get('/page/maps', (req, res) => res.render("maps"));
+app.get('/', (req, res) => res.render('homepage'));
+app.get('/page/login', (req, res) => res.render('login_page'));
+app.get('/page/register', (req, res) => res.render('register_page'));
+app.get('/page/maps', (req, res) => res.render('maps'));
 
 
-app.use(authRouter)
-app.use(videoRouter)
+app.use(authRouter);
+app.use(videoRouter);
 
 // middleware
 app.use(notFoundRout);
